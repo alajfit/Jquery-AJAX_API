8c6527da30d6bbdbcf95fc317d75ef99eaff0bec
Alan J. Fitzpatrick <alajfit@gmail.com>
2015-05-31 18:22:49 +0100

    Initial Setup of Order App/ To Do list

---
+++ app/scripts/main.js
@@ -0,0 +1,74 @@
+console.log('\'Allo \'Allo!');
+
+$(document).ready(function(){
+	'use strict';
+	// cache DOM elements
+	var $orders = $('#orders');
+	var $newName = $('#name');
+	var $newDrink = $('#drink');
+
+	var orderTemplate = ""+
+	"<li>"+
+	"<strong>Name:</strong> {{name}} | "+
+	"<strong>Drink:</strong> {{drink}}"+
+	"<button data-id='1' class='btn remove btn-danger pull-right'>x</button>"
+	"</li>";
+
+	function addOrder(order) {
+		// Pass Mustache a Template and an Object
+		$orders.append(Mustache.render(orderTemplate, order));
+	}
+
+	$.ajax({
+		type: 'GET',
+		url: '/api/orders.json',
+		success: function(orders) {
+			// we grab each element of the array
+			// Pass the array and a function call
+			// We then pass I as index and order as item
+			$.each(orders, function(i, order) {
+				addOrder(order);
+			});
+		},
+		error: function() {
+			alert('Error loading errors');
+		}
+	});
+
+	$('#add-order').on('click', function() {
+		var order = {
+			name: $newName.val(),
+			drink: $newDrink.val()
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: '/api/orders.json',
+			data: order,
+			success: function(newOrder) {
+				addOrder(newOrder);
+			},
+			error: function() {
+				alert('Error Posting!');
+			}
+		});
+	});
+
+	$orders.delegate('.remove', 'click', function() {
+		// creating a link to closest li
+		var $li = $(this).closest('li');
+		$.ajax({
+			type: 'DELETE',
+			url: '/api/orders/' + $(this).attr('data-id'),
+			success: function() {
+				$li.fadeOut(300, function(){
+					$(this).remove();
+				});
+				console.log('Removed');
+			},
+			error: function() {
+				alert("Didnt Work");
+			}
+		});
+	});
+});
